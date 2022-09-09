4a1677dc42ff5f2e785c7d156ec4b43a49e6dcac
sunnyntini <sentini@uncg.edu>
2022-09-09 18:01:30 -0400

    Added parsing JSON objects

--- src/main/java/com/csc340/RestAPI/RestApiController.java
+++ src/main/java/com/csc340/RestAPI/RestApiController.java
@@ -1,7 +1,11 @@
 package com.csc340.RestAPI;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,19 +23,56 @@ public class RestApiController {
         return String.format("Hello, %s!", name);
     }
 
-    @GetMapping("/countries")
-    public List<Object> getCountries() {
-        String url = "https://restcountries.com/v3.1/all";
+    /**
+     * Get a random quote from quotable and make it available at this endpoint.
+     *
+     * @return
+     */
+    @GetMapping("/quote")
+    public Object getQuote() {
+        String url = "https://api.quotable.io/random";
         RestTemplate restTemplate = new RestTemplate();
-        Object[] countryList = restTemplate.getForObject(url, Object[].class);
-        return Arrays.asList(countryList);
+        Object jSonQuote = restTemplate.getForObject(url, Object.class);
+
+        //Print the whole response to console.
+        String quote = restTemplate.getForObject(url, String.class);
+        //Parse out the most important info from the response.
+        JSONObject jo = new JSONObject(quote);
+        System.out.println(jo.toString());
+        String quoteAuthor = jo.getString("author");
+        String quoteContent = jo.getString("content");
+        System.out.println(quoteAuthor);
+        System.out.println(quoteContent);
+
+        return jSonQuote;
     }
 
+    /**
+     * Get a list of universities from hipolabs and parse the most important
+     * info to console.
+     *
+     * @return
+     */
     @GetMapping("/universities")
     public List<Object> getUniversities() {
-        String url = "http://universities.hipolabs.com/search";
+        String url = "http://universities.hipolabs.com/search?name=sports";
         RestTemplate restTemplate = new RestTemplate();
         Object[] universities = restTemplate.getForObject(url, Object[].class);
+
+        //Print the whole response to console
+        JSONArray ja = new JSONArray(universities);
+        System.out.println(ja.toString());
+
+        //Parse out relevant info from each entry in the response
+        Iterator<Object> iter = ja.iterator();
+        while (iter.hasNext()) {
+            JSONObject jo = (JSONObject) iter.next();
+            String name = jo.getString("name");
+            String country = jo.getString("country");
+            System.out.println(name + " : " + country);
+
+        }
+
         return Arrays.asList(universities);
     }
 
